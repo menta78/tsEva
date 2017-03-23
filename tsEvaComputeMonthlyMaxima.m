997fa42843263b7774a27aaad776024ca2cc230c
Lorenzo Mentaschi <mentalo@D01RI1402967.ies.jrc.it>
2017-03-23 15:50:45 +0100

    tsEvaComputeMonthlyMaxima: optimization

--- tsEvaComputeMonthlyMaxima.m
+++ tsEvaComputeMonthlyMaxima.m
@@ -1,27 +1,23 @@
 function [monthlyMax, monthlyMaxDate, monthlyMaxIndx] = tsEvaComputeMonthlyMaxima(timeAndSeries)
 
-timeStamps = timeAndSeries(:,1);
-srs = timeAndSeries(:,2);
-lsrs = length(srs);
-indexes = (1:1:lsrs)';
+  timeStamps = timeAndSeries(:,1);
+  srs = timeAndSeries(:,2);
 
-dtvec = datevec(timeStamps);
-dtvecMonth = dtvec(:,1:2);
-dtvecMonth = cat(2, dtvecMonth, ones(length(dtvec), 1));
-tsMonth = datenum(dtvecMonth);
-tsMonthUnique = unique(tsMonth);
+  tmvec = datevec(timeStamps);
+  yrs = tmvec(:,1);
+  mnts = tmvec(:,2);
+  mnttmvec = [yrs mnts];
+  valsIndxs = 1:numel(srs);
 
-dataByMonth = cellfun(@(tsmonth) srs(tsMonth == tsmonth), num2cell(tsMonthUnique), 'uniformoutput', false);
-indxByMonth = cellfun(@(tsmonth) indexes(tsMonth == tsmonth), num2cell(tsMonthUnique), 'uniformoutput', false);
-monthlyMax = cellfun(@(mnthData) max(mnthData), dataByMonth);
-monthlyMaxIndx0 = cellfun(@(imonth) find(dataByMonth{imonth} == monthlyMax(imonth)), num2cell(1:1:length(monthlyMax)), 'uniformoutput', false);
-monthlyMaxIndx0(cellfun(@(c) isempty(c), monthlyMaxIndx0)) = {1};
-monthlyMaxIndx0 = [monthlyMaxIndx0{:}];
-monthlyMaxIndx = cellfun(@(imonth) indxByMonth{imonth}(monthlyMaxIndx0(imonth)), num2cell(1:1:length(monthlyMax)));
+  function maxIndx = findMax(indxs)
+    [~, maxSubIndx] = max(srs(indxs));
+    maxIndx = indxs(maxSubIndx);
+  end
 
-monthlyMaxDate = timeStamps(monthlyMaxIndx);
+  monthlyMaxIndx = accumarray(mnttmvec, valsIndxs, [], @findMax);
+  monthlyMaxIndx = sort(monthlyMaxIndx(monthlyMaxIndx ~= 0));
+  monthlyMax = srs(monthlyMaxIndx);
+  monthlyMaxDate = timeStamps(monthlyMaxIndx);
 
-monthlyMax = monthlyMax';
-monthlyMaxDate = monthlyMaxDate';
-monthlyMaxIndx = monthlyMaxIndx';
+end
 
