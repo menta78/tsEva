0be15dca808e355fd837ef503963f8fc8415d0d1
Lorenzo Mentaschi <mentalo@D01RI1402967.ies.jrc.it>
2017-07-27 11:16:23 +0200

    tsTimeSeriesToPointData: set as series only the points beyond the input threshold. set to POTdata some missing values

--- tsTimeSeriesToPointData.m
+++ tsTimeSeriesToPointData.m
@@ -3,20 +3,23 @@ function pointData = tsTimeSeriesToPointData( ms, potThreshold, potThresholdErro
 % tsTimeSeriesToPointData: given a ms produces a structure pointData
 % like the one produced by tsEvaSampleData
 
-  pointData.completeSeries = ms;
+  ms1 = ms(ms(:,2) > potThreshold, :);
+  percentile = (1 - size(ms1, 1)/size(ms, 1))*100;
+
+  pointData.completeSeries = ms1;
   
   POTdata.threshold = potThreshold;
-  POTdata.thresholdError = 0;
-  POTdata.percentile = 0;
-  POTdata.peaks = ms(:,2);
-  POTdata.ipeaks=1:size(ms, 1);
-  POTdata.sdpeaks=ms(:,1);  
+  POTdata.thresholdError = potThresholdError;
+  POTdata.percentile = percentile;
+  POTdata.peaks = ms1(:,2);
+  POTdata.ipeaks=1:size(ms1, 1);
+  POTdata.sdpeaks=ms1(:,1);  
   pointData.POT = POTdata;
   
-  [pointData.annualMax, pointData.annualMaxTimeStamp, pointData.annualMaxIndexes] = tsEvaComputeAnnualMaxima(ms);
-  [pointData.monthlyMax, pointData.monthlyMaxTimeStamp, pointData.monthlyMaxIndexes] = tsEvaComputeMonthlyMaxima(ms);
+  [pointData.annualMax, pointData.annualMaxTimeStamp, pointData.annualMaxIndexes] = tsEvaComputeAnnualMaxima(ms1);
+  [pointData.monthlyMax, pointData.monthlyMaxTimeStamp, pointData.monthlyMaxIndexes] = tsEvaComputeMonthlyMaxima(ms1);
   
-  yrs = unique(tsYear(ms(:,1)));
+  yrs = unique(tsYear(ms1(:,1)));
   yrs = yrs - min(yrs);
   pointData.years = (nanmin(yrs):1:nanmax(yrs))';
   
