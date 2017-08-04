9e18315a753e0a46ac88f0121ddf9528a0863312
Lorenzo Mentaschi <mentalo@D01RI1402967.ies.jrc.it>
2017-08-04 16:24:32 +0200

    tsGetReturnPeriodOfLevel: il loglog interpolation fails, setting to infinite the return period of too high return levels

--- tsGetReturnPeriodOfLevel.m
+++ tsGetReturnPeriodOfLevel.m
@@ -20,9 +20,30 @@ function [ myRetPeriod, myRetPeriodCISup, myRetPeriodCIInf ] = tsGetReturnPeriod
   end
   
   myLevNonNan = ~isnan(myLevel);
-  myRetPeriod_ = tsInterp1Extrap(retLevel, retPeriod, myLevel(myLevNonNan), logExtrap)';
-  myRetPeriodCISup_ = tsInterp1Extrap(rlLow, retPeriod, myLevel(myLevNonNan), logExtrap)';
-  myRetPeriodCIInf_ = tsInterp1Extrap(rlHigh, retPeriod, myLevel(myLevNonNan), logExtrap)';
+
+  try
+    myRetPeriod_ = tsInterp1Extrap(retLevel, retPeriod, myLevel(myLevNonNan), logExtrap)';
+  catch
+    disp('    ... log extrapolation of return failed for some levels. Setting it to infinite.');
+    myRetPeriod_ = interp1(retLevel, retPeriod, myLevel(myLevNonNan));
+    myRetPeriod_(isnan(myRetPeriod_)) = inf;
+  end
+  
+  try
+    myRetPeriodCISup_ = tsInterp1Extrap(rlLow, retPeriod, myLevel(myLevNonNan), logExtrap)';
+  catch
+    disp('    ... log extrapolation of return period superior bar failed for some levels. Setting there an infinite superior ci.');
+    myRetPeriodCISup_ = interp1(rlLow, retPeriod, myLevel(myLevNonNan));
+    myRetPeriodCISup_(isnan(myRetPeriodCISup_)) = inf;
+  end
+  
+  try
+    myRetPeriodCIInf_ = tsInterp1Extrap(rlHigh, retPeriod, myLevel(myLevNonNan), logExtrap)';
+  catch
+    disp('    ... log extrapolation of return period superior bar failed for some levels. Setting there an infinite superior ci.');
+    myRetPeriodCIInf_ = interp1(rlHigh, retPeriod, myLevel(myLevNonNan));
+    myRetPeriodCIInf_(isnan(myRetPeriodCIInf_)) = inf;
+  end
   
   myRetPeriod = ones(size(myLevel))*nan;
   myRetPeriod(myLevNonNan) = myRetPeriod_;
