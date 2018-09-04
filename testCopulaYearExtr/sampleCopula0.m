70ac546b71ec3235cd9a17d3b224a06fa971f905
Lorenzo Mentaschi <lorenzo.mentaschi@ec.europa.eu>
2018-09-04 14:57:32 +0200

    copula: test updated with a copula btwn neighboring points

--- testCopulaYearExtr/sampleCopula0.m
+++ testCopulaYearExtr/sampleCopula0.m
@@ -12,3 +12,22 @@ figHnd = tsCopulaYearExtrPlotSctrBivar(resampleLevel, yMax, 'xlbl', '-SPEI', 'yl
 title(['Milan, joint distribution ' newline ' of extreme SPEI and TMax']);
 
 saveas(figHnd(1), 'milan_SpeiTmaxJointDist.png');
+
+
+
+clear all;
+
+load('testESL_closePts_bivariate_Copula.mat');
+
+disp('testing stat. copula for yearly extr. sea level, for 2 close locations in Portugal');
+disp(['at lon, lat = ' num2str(xtst) ', ' num2str(ytst)]);
+
+[retLev, jdist, cplParam] = tsCopulaYearExtrFit(retPeriod, retLev, retLevError, yMax, 'copulafamily', 'gumbel');
+
+nResample = 10000;
+[resampleLevel, resampleProb] = tsCopulaYearExtrRnd(retPeriod, retLev, cplParam, nResample);
+
+figHnd = tsCopulaYearExtrPlotSctrBivar(resampleLevel, yMax, 'xlbl', 'Location 1', 'ylbl', 'Location 2');
+title(['joint distribution ' newline ' of extr. sea lvl at 2 close locations in Portugal']);
+
+saveas(figHnd(1), 'testESL_closePts_bivariate_Copula.png');
