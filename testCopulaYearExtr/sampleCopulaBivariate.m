65e0ada866a53c0f7443084680968a85672af75a
Lorenzo Mentaschi <lorenzo.mentaschi@ec.europa.eu>
2018-09-12 12:16:32 +0200

    sampleCopulaBivariate: added the spearman correlation and kendall tau as examples of test

--- testCopulaYearExtr/sampleCopulaBivariate.m
+++ testCopulaYearExtr/sampleCopulaBivariate.m
@@ -5,10 +5,16 @@ load('testSpeiTmaxCopula.mat');
 disp('testing stationary copula for yearly extremes of SPEI (droughts) and TMax');
 disp(['at lon, lat = ' num2str(xtst) ', ' num2str(ytst)]);
 
+kendallTauInput = corr(yMax, 'type', 'kendall');
+spearmanCorrInput = corr(yMax, 'type', 'spearman');
+
 [retLev, jdist, cplParam] = tsCopulaYearExtrFit(retPeriod, retLev, yMax, 'copulafamily', 'gumbel');
 
-nResample = 10000;
+nResample = 100000;
 [resampleLevel, resampleProb] = tsCopulaYearExtrRnd(retPeriod, retLev, cplParam, nResample);
+%skipping the computation of the kendall tau for the output, as it is very slow
+% kendallTauOutput = corr(resampleLevel, 'type', 'kendall');
+spearmanCorrOutput = corr(resampleLevel, 'type', 'spearman');
 
 figHnd = tsCopulaYearExtrPlotSctrBivar(resampleLevel, yMax, 'xlbl', '-SPEI', 'ylbl', 'TMax');
 title(['Milan, joint distribution ' newline ' of extreme SPEI and TMax']);
@@ -24,10 +30,16 @@ load('testESL_closePts_bivariate_Copula.mat');
 disp('testing stat. copula for yearly extr. sea level, for 2 close locations in Portugal');
 disp(['at lon, lat = ' num2str(xtst) ', ' num2str(ytst)]);
 
+kendallTauInput = corr(yMax, 'type', 'kendall');
+spearmanCorrInput = corr(yMax, 'type', 'spearman');
+
 [retLev, jdist, cplParam] = tsCopulaYearExtrFit(retPeriod, retLev, yMax, 'copulafamily', 'gumbel');
 
 nResample = 10000;
 [resampleLevel, resampleProb] = tsCopulaYearExtrRnd(retPeriod, retLev, cplParam, nResample);
+%skipping the computation of the kendall tau for the output, as it is very slow
+% kendallTauOutput = corr(resampleLevel, 'type', 'kendall');
+spearmanCorrOutput = corr(resampleLevel, 'type', 'spearman');
 
 figHnd = tsCopulaYearExtrPlotSctrBivar(resampleLevel, yMax, 'xlbl', 'Location 1', 'ylbl', 'Location 2');
 title(['joint distribution ' newline ' of extr. sea lvl at 2 close locations in Portugal']);
