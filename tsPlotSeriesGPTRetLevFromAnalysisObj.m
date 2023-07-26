c18a01ccd4fd8ce8709c575eaf6c134fd302c2aa
Lorenzo Mentaschi <lorenzo.mentaschi@unibo.it>
2023-07-26 13:32:17 +0200

    tsPlotSeriesGPTRetLevFromAnalysisObj.m fix: the return levels are computed inside of the function, no need to pass them as parameters

--- tsPlotSeriesGPTRetLevFromAnalysisObj.m
+++ tsPlotSeriesGPTRetLevFromAnalysisObj.m
@@ -2,59 +2,42 @@
 %%well as peaks
 
 function phandles = tsPlotSeriesGPTRetLevFromAnalysisObj( nonStationaryEvaParams, stationaryTransformData, varargin )
-args.plotPercentile = -1;
-args = tsEasyParseNamedArgs(varargin, args);
-plotPercentile = args.plotPercentile;
-  
+
 timestamps = stationaryTransformData.timeStamps;
 series = stationaryTransformData.nonStatSeries;
-trend = stationaryTransformData.trendSeries;
-stdDev = stationaryTransformData.stdDevSeries;
-if isfield(stationaryTransformData, 'statsTimeStamps')
-  statsTimeStamps = stationaryTransformData.statsTimeStamps;
-else
-  statsTimeStamps = timestamps;
-end
 
 args.legendLocation = 'northwest';
 args.rlevel=1;
 args.pks=1;
 
-args = tsEasyParseNamedArgs(varargin, args);
-rlevel=args.rlevel
+% computing the return levels
+epsilon = nonStationaryEvaParams(2).parameters.epsilon;
+sigma = nonStationaryEvaParams(2).parameters.sigma;
+threshold = nonStationaryEvaParams(2).parameters.threshold;
+thStart =  nonStationaryEvaParams(2).parameters.timeHorizonStart;
+thEnd = nonStationaryEvaParams(2).parameters.timeHorizonEnd;
+timeHorizonInYears = (thEnd - thStart)/365.2425;
+nPeaks = nonStationaryEvaParams(2).parameters.nPeaks;
+epsilonStdErr = nonStationaryEvaParams(2).paramErr.epsilonErr;
+sigmaStdErr = nonStationaryEvaParams(2).paramErr.sigmaErr;
+thresholdStdErr = nonStationaryEvaParams(2).paramErr.thresholdErr;
+returnPeriods = [5, 10, 30, 100];
+[rlevel, retrunLevelsErrs] = tsEvaComputeReturnLevelsGPD(epsilon, sigma, threshold, epsilonStdErr, sigmaStdErr, thresholdStdErr, nPeaks, timeHorizonInYears, returnPeriods);
 
-% phandles = tsEvaPlotSeriesTrendStdDev(timestamps, series, trend,stdDev, 'statsTimeStamps', statsTimeStamps, varargin{:});
-
-% 
-% if plotPercentile ~= -1
-%   prcntile = tsEvaNanRunningPercentile(series, stationaryTransformData.runningStatsMulteplicity, plotPercentile);
-%   figure(phandles{1});
-%   hold on;
-%   hndl = plot(timestamps, prcntile);
-%   phandles{length(phandles) + 1} = hndl;
-% end
-
-hold off;
-
-figure
+phandles{1} = figure;
 set(gcf,'position',[87.4000  342.0000  960.6000  420.0000])
-plot(timestamps,series)
-hold on
+plot(timestamps,series);
+hold on;
 % line([timestamps(1) timestamps(end)],rlevel(1)*ones(1,2),'Color','r')
-plot(timestamps,rlevel(:,1),'Color','r')
-plot(timestamps,rlevel(:,2),'Color','g')
-plot(timestamps,rlevel(:,3),'Color','b')
-plot(timestamps,rlevel(:,4),'Color','k')
-% line([timestamps(1) timestamps(end)],rlevel(2)*ones(1,2),'Color','g')
-% line([timestamps(1) timestamps(end)],rlevel(3)*ones(1,2),'Color','b')
-% line([timestamps(1) timestamps(end)],rlevel(4)*ones(1,2),'Color','k')
-pks=args.pks;
-phandles=plot(timestamps(pks),series(pks),'*')
-%  xlim([timestamps(1)-6*30 timestamps(end)+48*30])
-%  ylim([min(series)-0.5 max(series)+1.2])
- datetick('x',12)
-  legend('Series','5','10','30','100','peaks','location','bestoutside')
-xlabel('Date (time)')
-ylabel('TWL (m)')
+phandles{2} = plot(timestamps,rlevel(:,1),'Color','r');
+phandles{3} = plot(timestamps,rlevel(:,2),'Color','g');
+phandles{4} = plot(timestamps,rlevel(:,3),'Color','b');
+phandles{5} = plot(timestamps,rlevel(:,4),'Color','k');
+pks = nonStationaryEvaParams(2).objs.peakIndexes;
+phandles{6} = plot(timestamps(pks),series(pks),'*');
+datetick('x',12);
+legend('Series','5','10','30','100','peaks','location','bestoutside');
+xlabel('Date (time)');
+ylabel('TWL (m)');
 end
 
