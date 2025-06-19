cf460036742bec73e5bcb95497c456d680590432
Lorenzo Mentaschi <lorenzo.mentaschi@unibo.it>
2025-06-19 16:40:05 +0200

    fix in tsGpdNegShapeFit

--- tsGpdNegShapeFit.m
+++ tsGpdNegShapeFit.m
@@ -16,26 +16,11 @@ function [paramEsts, paramCIs] = tsGpdNegShapeFit(data, alphaCI)
 
     % Fit using MLE with constraint k < 0
     % [paramEsts, ~, exitflag, output, ~, hessian] 
-    paramEsts = mle(data, ...
+    [paramEsts, paramCIs] = mle(data, ...
         'pdf', gpdPdf, ...
         'start', [startK, startSigma], ...
         'lowerbound', [-Inf, 0], ...
         'upperbound', [0, Inf], ...
+        'alpha', alphaCI, ...
         'options', statset('MaxIter', 1e4, 'Display', 'off'));
-    % covMatrix could be computed as hessian^-1 using the fact that dt ~ t-E(t) 
-    covMatrix = mlecov(paramEsts, data, 'pdf', gpdPdf);
-
-    % Compute standard errors
-    se = sqrt(diag(covMatrix));
-
-    % Compute confidence intervals
-    z = norminv(1 - alphaCI / 2);
-    k = paramEsts(1);
-    sigma = paramEsts(2);
-    kCI = [k - z * se(1), k + z * se(1)];
-    sigmaCI = [sigma - z * se(2), sigma + z * se(2)];
-
-    % Output structure matches gpfit
-    paramCIs = [kCI; sigmaCI]';
-
 end
